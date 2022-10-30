f78142b01a0d3d67dc388c69dab88c93d1864d1e
Soren Laney <soren@laneymedia.org>
2022-10-30 14:26:49 -0600

    Streamlit App Update

---
+++ Streamlit App/laneylytics.py
@@ -0,0 +1,12 @@
+from git import Head
+import streamlit as st
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+modelTraining = st.beta_container()
+
+
+with Header:
+    st.title('Laney Media Analytics Dashboard')
+
