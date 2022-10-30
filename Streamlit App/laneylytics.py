740c7fda9bd99706668f9a6b9810c215622d69e4
Soren Laney <soren@laneymedia.org>
2022-10-30 17:32:10 -0600

    app and clickup api updates

--- Streamlit App/laneylytics.py
+++ Streamlit App/laneylytics.py
@@ -1,12 +1,28 @@
 from git import Head
 import streamlit as st
+import pandas as pd
 
-header = st.beta_container()
-dataset = st.beta_container()
-features = st.beta_container()
-modelTraining = st.beta_container()
+header = st.container()
+dataset = st.container()
+features = st.container()
+model_training = st.container()
 
 
-with Header:
+with header:
     st.title('Laney Media Analytics Dashboard')
+    
 
+with dataset:
+    st.header('Content Plan Dataset')
+    st.text(' test test test')
+
+    #clickup_data = pd.read_csv('')
+    #st.write(clickup_data.head())
+
+with features:
+    st.header('Best Preforming Content')
+
+
+
+with model_training:
+    st.header('Predicting Views')
